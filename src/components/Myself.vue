0b94a26e752f58b3961499b297d4773badd6345e
tboun <tboun@Apple.router.taotronics.com>
2022-09-12 22:45:16 -0700

    fixing the home page

--- src/components/Myself.vue
+++ src/components/Myself.vue
@@ -2,8 +2,8 @@
     <div class="flex flex-col gap-8">
       <h1
         class="text-xl sm:text-3xl md:text-4xl md:leading-normal slg:text-5xl slg:leading-relaxed font-theme_bold text-head_text"
-      >
-        Working on polishing my skills as a future Web Developer<span
+      >Hi, my name is Tary
+        <span
           class="text-link_text_hover"
           >.</span
         >
@@ -11,14 +11,14 @@
       <p
         class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
       >
-        My name is Tary
+        I am currently working on polishing my skills as a future Web Developer
       </p>
       <p
         class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
       >
-        I'm currently working on a
+        
         <LinkBtn link="https://google.com/"></LinkBtn>, 
-        <LinkBtn link="https://vite-psi-three.vercel.app/">website</LinkBtn
+        <LinkBtn link="https://tarybport.vercel.app/">website</LinkBtn
         >,
       </p>
     </div>
