487d4427731d9d9a4870d25081852d3472e08fb1
tboun <tboun@Apple.router.taotronics.com>
2022-09-15 00:59:52 -0700

    changed nav bar headings

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,12 +2,11 @@
   
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-        <a href="https://flowbite.com" class="flex items-center">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Tary B</span>
+        <a href="https://taryb.xyz" class="flex items-center">
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Tary B.</span>
         </a>
         <div class="flex items-center">
-          <router-link to="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Me</router-link>
-   </div>
+          <router-link to="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Me</router-link>        </div>
     </div>
 </nav>
 <nav class="bg-gray-50 dark:bg-gray-700">
@@ -24,6 +23,7 @@
       <li>
   <router-link to="/projects" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</router-link>
  </li>
+ 
  
 
             </ul>
